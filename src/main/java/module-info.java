c6c97b014b0b6d2e3f6273157988ea488dd39955
Voevodin_Roman <q556677@yandex.ru>
2022-06-07 16:53:38 +0400

    Add file to VCS

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module voevodin.ru.gb.womework2_4 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens voevodin.ru.gb.womework2_4 to javafx.fxml;
+    exports voevodin.ru.gb.womework2_4;
+}
---
+++ src/main/java/voevodin/ru/gb/womework2_4/HelloController.java
@@ -0,0 +1,14 @@
+package voevodin.ru.gb.womework2_4;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
