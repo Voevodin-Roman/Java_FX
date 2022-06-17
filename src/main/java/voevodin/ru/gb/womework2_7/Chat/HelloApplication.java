d81aad815a190dae04a344788b5ee50f0c3fcc78
Voevodin_Roman <q556677@yandex.ru>
2022-06-17 10:00:42 +0400

    Add Chat

--- src/main/java/voevodin/ru/gb/womework2_7/Chat/HelloApplication.java
+++ src/main/java/voevodin/ru/gb/womework2_7/Chat/HelloApplication.java
@@ -1,4 +1,4 @@
-package voevodin.ru.gb.womework2_4;
+package voevodin.ru.gb.womework2_7;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -11,8 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
-        stage.setTitle("Мой чат");
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
@@ -20,4 +20,5 @@ public class HelloApplication extends Application {
     public static void main(String[] args) {
         launch();
     }
+}
 }
